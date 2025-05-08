3d047a418d650034485c68109a8f623fe07e02ff
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2025-05-08 12:17:54 +0200

    Third homework

--- project4_TriangularMesh/project4.js
+++ project4_TriangularMesh/project4.js
@@ -3,40 +3,36 @@
 // It returns the combined 4x4 transformation matrix as an array in column-major order.
 // The given projection matrix is also a 4x4 matrix stored as an array in column-major order.
 // You can use the MatrixMult function defined in project4.html to multiply two 4x4 matrices in the same format.
-function GetModelViewProjection( projectionMatrix, translationX, translationY, translationZ, rotationX, rotationY ){
-	// RotX
-	var cosX = Math.cos(rotationX);
-	var sinX = Math.sin(rotationX);
-	var rotX = [
-		1,    0,     0, 0,
+// === MATRIX ===
+function GetModelViewProjection(projectionMatrix, translationX, translationY, translationZ, rotationX, rotationY) {
+	const cosX = Math.cos(rotationX);
+	const sinX = Math.sin(rotationX);
+	const cosY = Math.cos(rotationY);
+	const sinY = Math.sin(rotationY);
+
+	const rotX = [
+		1, 0, 0, 0,
 		0, cosX, sinX, 0,
 		0, -sinX, cosX, 0,
-		0,    0,     0, 1
+		0, 0, 0, 1
 	];
 
-	// RotY
-	var cosY = Math.cos(rotationY);
-	var sinY = Math.sin(rotationY);
-	var rotY = [
+	const rotY = [
 		cosY, 0, -sinY, 0,
-		0, 1,     0, 0,
-		sinY, 0,  cosY, 0,
-		0, 0,     0, 1
+		0, 1, 0, 0,
+		sinY, 0, cosY, 0,
+		0, 0, 0, 1
 	];
 
-	// Translation
-	var trans = [
+	const trans = [
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		translationX, translationY, translationZ, 1
 	];
 
-	// T * Ry * Rx
-	var mv = MatrixMult( trans, MatrixMult( rotY, rotX ) );
-	var mvp = MatrixMult(projectionMatrix, mv); // Use projectionMatrix
-	
-	return mvp;
+	const mv = MatrixMult(trans, MatrixMult(rotY, rotX));
+	return MatrixMult(projectionMatrix, mv);
 }
 
 
@@ -44,71 +40,79 @@ function GetModelViewProjection( projectionMatrix, translationX, translationY, t
 const meshVS = `
     attribute vec3 pos;
     attribute vec2 texCoord;
+
     uniform mat4 mvp;
+    uniform mat4 yzSwap;
+
     varying vec2 vTexCoord;
 
     void main() {
-        gl_Position = mvp * vec4(pos, 1.0);
+        gl_Position = mvp * yzSwap * vec4(pos, 1.0);
         vTexCoord = texCoord;
     }
 `;
 
+
 const meshFS = `
     precision mediump float;
-    varying vec2 vTexCoord;
+
     uniform sampler2D tex;
     uniform bool useTexture;
 
+    varying vec2 vTexCoord;
+
     void main() {
         if (useTexture) {
-            vec2 tiledCoord = fract(vTexCoord);
+            vec2 tiledCoord = fract(vTexCoord * 2.0);
             gl_FragColor = texture2D(tex, tiledCoord);
         } else {
             gl_FragColor = vec4(1.0, gl_FragCoord.z * gl_FragCoord.z, 0.0, 1.0);
-        }
+        } 
     }
 `;
 
 
 class MeshDrawer {
-    constructor() {
-        // Compile shaders
-        this.prog = InitShaderProgram(meshVS, meshFS);
-        this.mvp = gl.getUniformLocation(this.prog, 'mvp');
-        this.sampler = gl.getUniformLocation(this.prog, 'tex');
-        this.useTexture = gl.getUniformLocation(this.prog, 'useTexture');
+	constructor() {
+		this.prog = InitShaderProgram(meshVS, meshFS);
+		this.mvpLoc = gl.getUniformLocation(this.prog, 'mvp');
+		this.sampler = gl.getUniformLocation(this.prog, 'tex');
+		this.useTextureLoc = gl.getUniformLocation(this.prog, 'useTexture');
+		this.yzSwapLoc = gl.getUniformLocation(this.prog, 'yzSwap');
 
-        // Attribute locations
-        this.vertPos = gl.getAttribLocation(this.prog, 'pos');
-        this.texCoord = gl.getAttribLocation(this.prog, 'texCoord');
+		this.posLoc = gl.getAttribLocation(this.prog, 'pos');
+		this.texLoc = gl.getAttribLocation(this.prog, 'texCoord');
 
-        // Buffers
-        this.vertbuffer = gl.createBuffer();
-        this.texbuffer = gl.createBuffer();
+		this.vertBuf = gl.createBuffer();
+		this.texBuf = gl.createBuffer();
 
-        // Texture
-        this.texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		this.texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
-        // Defaults
-        this.numTriangles = 0;
-        this.hasTexture = false;
-    }
+		this.numTriangles = 0;
+		this.hasTexture = false;
+		this.hasTexCoords = false;
+
+		this.yzSwapMat = [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		];
+	}
 
 	setMesh(vertPos, texCoords) {
 		this.numTriangles = vertPos.length / 3;
-	
-		// Vertex positions
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertbuffer);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuf);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.STATIC_DRAW);
-	
-		// Texture coordinates
+
 		if (texCoords && texCoords.length > 0) {
-			gl.bindBuffer(gl.ARRAY_BUFFER, this.texbuffer);
+			gl.bindBuffer(gl.ARRAY_BUFFER, this.texBuf);
 			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
 			this.hasTexCoords = true;
 		} else {
@@ -116,43 +120,59 @@ class MeshDrawer {
 		}
 	}
 
-    setTexture(img) {
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); // flip the image
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-                      gl.UNSIGNED_BYTE, img);
-        this.hasTexture = true;
-    }
+	setTexture(img) {
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+		this.hasTexture = true;
+	}
 
-draw(matrixMVP) {
-    gl.useProgram(this.prog);
-    gl.uniformMatrix4fv(this.mvp, false, matrixMVP);
-    gl.uniform1i(this.useTexture, this.hasTexture && this.hasTexCoords);
+	showTexture(show) {
+		gl.useProgram(this.prog);
+		gl.uniform1i(this.useTextureLoc, show ? 1 : 0);
+	}
 
-    if (this.hasTexture && this.hasTexCoords) {
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.uniform1i(this.sampler, 0);
-    }
+	swapYZ(swap) {
+		gl.useProgram(this.prog);
+		if (swap) {
+			this.yzSwapMat = MatrixMult(
+				[1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+				[1, 0, 0, 0, 0, Math.cos(Math.PI/2), Math.sin(Math.PI/2), 0,
+				 0, -Math.sin(Math.PI/2), Math.cos(Math.PI/2), 0, 0, 0, 0, 1]
+			);
+		} else {
+			this.yzSwapMat = [
+				1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1
+			];
+		}
+	}
 
-    // Position attribute
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertbuffer);
-    gl.enableVertexAttribArray(this.vertPos);
-    gl.vertexAttribPointer(this.vertPos, 3, gl.FLOAT, false, 0, 0);
+	draw(matrixMVP) {
+		gl.useProgram(this.prog);
+		gl.uniformMatrix4fv(this.mvpLoc, false, matrixMVP);
+		gl.uniformMatrix4fv(this.yzSwapLoc, false, this.yzSwapMat);
+		gl.uniform1i(this.useTextureLoc, this.hasTexture && this.hasTexCoords);
 
-    // Texture attribute
-    if (this.hasTexCoords) {
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.texbuffer);
-        gl.enableVertexAttribArray(this.texCoord);
-        gl.vertexAttribPointer(this.texCoord, 2, gl.FLOAT, false, 0, 0);
-    }
+		if (this.hasTexture && this.hasTexCoords) {
+			gl.activeTexture(gl.TEXTURE0);
+			gl.bindTexture(gl.TEXTURE_2D, this.texture);
+			gl.uniform1i(this.sampler, 0);
+		}
 
-    gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuf);
+		gl.enableVertexAttribArray(this.posLoc);
+		gl.vertexAttribPointer(this.posLoc, 3, gl.FLOAT, false, 0, 0);
 
-	console.log("Drawing", this.numTriangles, "triangles");
-	console.log("Has texture:", this.hasTexture);
-console.log("TexCoord attrib:", this.texCoord);
+		if (this.hasTexCoords) {
+			gl.bindBuffer(gl.ARRAY_BUFFER, this.texBuf);
+			gl.enableVertexAttribArray(this.texLoc);
+			gl.vertexAttribPointer(this.texLoc, 2, gl.FLOAT, false, 0, 0);
+		}
 
+		gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles);
+	}
 }
 
-}
